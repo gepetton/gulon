8dbd871a3f25b19dbbaa726bbca14d024e5d4ff1
Sehyeon Park <sehyeon73@gmail.com>
2025-06-19 18:14:58 +0900

    init

---
+++ src/main/java/com/gulon/app/GulonApplication.java
@@ -0,0 +1,13 @@
+package com.gulon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulonApplication.class, args);
+    }
+
+}
